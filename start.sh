181a22c8f4822309fa7d3fda245aaafd444a7fa9
kirillyesipov <134583932+kirillyesipov@users.noreply.github.com>
2024-12-21 14:12:23 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,28 @@
-#!/bin/sh -ex
+# #!/bin/sh -ex
 
 # This file is how Fly starts the server (configured in fly.toml). Before starting
 # the server though, we need to run any prisma migrations that haven't yet been
 # run, which is why this file exists in the first place.
 # Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
 
-npm run start
+# npm run start
+
+#!/bin/bash
+
+# Wait for Supabase to be ready
+until pg_isready -h supabase -p 5432 -U postgres; do
+  echo "Waiting for Supabase to be ready..."
+  sleep 2
+done
+
+echo "Supabase is ready. Running migrations..."
+
+# Run Prisma migrations
+npx prisma migrate deploy
+
+echo "Starting application..."
+
+# Start the application
+exec node /src/build
+
+exec npm run start
